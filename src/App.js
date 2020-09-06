794d8451c77fc1f3a156a552cfdd23ffd8e2483e
kevin <kevinrangels>
2020-09-05 21:18:39 -0430

    comments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import { Button, Input } from "@material-ui/core";
 import ImageUpload from "./ImageUpload";
+import InstagramEmbed from "react-instagram-embed";
 
 function getModalStyle() {
   const top = 50;
@@ -34,7 +35,7 @@ function App() {
   const [modalStyle] = React.useState(getModalStyle);
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
-  const [openSingIn, setOpenSingIn]  = useState(false);
+  const [openSingIn, setOpenSingIn] = useState(false);
 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
@@ -42,58 +43,58 @@ function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribe =  auth.onAuthStateChanged((authUser) =>{
-      if(authUser) {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
         // user has logged in ..
         setUser(authUser);
         console.log(authUser);
-      }else{
+      } else {
         //user has logget out..
         setUser(null);
       }
-    })
+    });
     return () => {
       unsubscribe();
-    }
-  },[user, username]);
+    };
+  }, [user, username]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot(snapshot => {
-      setPosts(
-        snapshot.docs.map(doc => ({
-          id: doc.id,
-          post: doc.data()
-        }))
-      );
-    });
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot(snapshot => {
+        setPosts(
+          snapshot.docs.map(doc => ({
+            id: doc.id,
+            post: doc.data()
+          }))
+        );
+      });
   }, []);
 
   const signUp = event => {
     event.preventDefault();
-    auth.createUserWithEmailAndPassword(email, password)
-    .then((authUser) => {
-      return authUser.user.updateProfile({
-        displayName: username
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(authUser => {
+        return authUser.user.updateProfile({
+          displayName: username
+        });
       })
-    })
-    .catch((error) => alert(error.message))
+      .catch(error => alert(error.message));
     setOpen(false);
   };
 
-  const signIn = (event) => {
+  const signIn = event => {
     event.preventDefault();
     auth
       .signInWithEmailAndPassword(email, password)
-      .catch((error) => alert(error.message))
+      .catch(error => alert(error.message));
 
-      setOpenSingIn(false);
-  }
+    setOpenSingIn(false);
+  };
 
   return (
     <div className="App">
-
-    <ImageUpload/>
-
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
           <form className="app__signup">
@@ -126,7 +127,9 @@ function App() {
               onChange={e => setPassword(e.target.value)}
             />
 
-            <Button type="submit" onClick={signUp}>Sing Up</Button>
+            <Button type="submit" onClick={signUp}>
+              Sing Up
+            </Button>
           </form>
         </div>
       </Modal>
@@ -156,40 +159,63 @@ function App() {
               onChange={e => setPassword(e.target.value)}
             />
 
-            <Button type="submit" onClick={signIn}>Sing Up</Button>
+            <Button type="submit" onClick={signIn}>
+              Sing Up
+            </Button>
           </form>
         </div>
       </Modal>
-
-
 
       <div className="app__header">
         <img
           className="app_headerImage"
           src="https://instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
         />
+        {user ? (
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSingIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign up</Button>
+          </div>
+        )}
       </div>
-      {user ?  (
-        <Button onClick={() => auth.signOut()}>Logout</Button>
-      ): (
-        <div className="app__loginContainer">
-          <Button onClick={() => setOpenSingIn(true)}>Sign In</Button>
-          <Button onClick={() => setOpen(true)}>Sign up</Button>
+
+      <div className="app__posts">
+        <div className="app__postsLeft">
+          {posts.map(({ id, post }) => (
+            <Post
+              key={id}
+              postId={id}
+              user={user}
+              username={post.username}
+              caption={post.caption}
+              imageUrl={post.imageUrl}
+            />
+          ))}
         </div>
+
+        <div className="app__postsRigth">
+          <InstagramEmbed
+            url="https://www.instagram.com/p/B2EVA6khexr/"
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName="div"
+            protocol=""
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          />
+        </div>
+      </div>
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <h3>Sorry you need to login to upload</h3>
       )}
-      
-
-      {posts.map(({ id, post }) => (
-        <Post
-          key={id}
-          username={post.username}
-          caption={post.caption}
-          imageUrl={post.imageUrl}
-        />
-      ))}
-
-      {/* Header */}
-      {/* Posts */}
     </div>
   );
 }
--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { Button } from "@material-ui/core";
+import { storage, db } from "./firebase";
+import firebase from "firebase"
+import "./ImageUpload.css";
 
-function ImageUpload() {
+function ImageUpload({username}) {
   const [image, setImage] = useState(null);
   const [url, setUrl] = useState("");
   const [progress, setProgress] = useState(0);
@@ -14,11 +17,46 @@ function ImageUpload() {
   }
 
   const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    
+    uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+            // progress function ...
+            const progress = Math.round(
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            );
+            setProgress(progress);
+        },
+        (error) => {
+            console.log(error);
+            alert(error.message)
+        },
+        () => {
+            storage
+            .ref("images")
+            .child(image.name)
+            .getDownloadURL()
+            .then(url => {
+                db.collection("posts").add({
+                  timestamp:  firebase.firestore.FieldValue.serverTimestamp(),
+                  caption: caption,
+                  imageUrl: url,
+                  username: username
 
+                });
+                setProgress(0);
+                setCaption("");
+                setImage(null);
+            })
+        }
+
+    )
   }
 
   return (
-    <div>
+    <div className="imageUpload">
+        <progress className="imageUpload__progress" value={progress} max="100" />
         <input type="text" placeholder='Enter a caption...' onChange={event => setCaption(event.target.value)} value={caption} />
         <input type="file" onChange={handleChange} />
         <Button className="imageUpload__button" onClick={handleUpload}>Upload</Button>
